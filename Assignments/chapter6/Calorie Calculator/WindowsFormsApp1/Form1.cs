724b3bc233fbf899aa0cea9381895f98801e4465
MashaTipikin <47071172+MashaTipikin@users.noreply.github.com>
2019-03-22 10:22:17 -0500

    chapter 6

---
+++ Assignments/chapter6/Calorie Calculator/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e) {
+            int fatInput;
+            int carbInput;
+            int fatCalorieTotal;
+            int carbCalorieTotal;
+
+            fatInput = Int32.Parse(FatCalorieBox.Text);
+            carbInput = Int32.Parse(CarbCalorieBox.Text);
+
+            fatCalorieTotal = fatInput * 9;
+            carbCalorieTotal = carbInput * 4;
+
+            fatCalorieLabel.Text = fatCalorieTotal.ToString();
+            carbCalorieLabel.Text = carbCalorieTotal.ToString();
+        }
+
+        private void Exit_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+    }
+}
